41d2ddfa416387bb374e3e8e14549f8b87bb26a4
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2026-02-26 10:29:15 +0530

    feat: add basic pages

--- app/hello.tsx
+++ app/hello.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Hello() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-red-500">
-        Edit app/index.tsx to edit this screen.
+        Edit app/hello.tsx to edit this screen.
       </Text>
     </View>
   );
